6b232707947815d6acd3a62d6518f69f9fb98bf6
Quentin Lopes <quentin.lopes@orange.fr>
2024-11-29 13:49:17 +0100

    calibrage pour jeu

--- Class.cpp
+++ Class.cpp
@@ -177,7 +177,7 @@ class Wizard : public Character {
 public :
 	Wizard() : Character("William l'intervenant", 225, 625, 500) {}
 	void attack(Entities& target) override {
-		int damage = 500;
+		int damage = 30;
 		target.losePV(damage);
 		cout << target.getHealth() << " PV" << endl << endl;
 		cout << c_name << " lance une attaque sur " << target.getName() << " lui infligeant " << damage << " points de degats " << endl << endl;
